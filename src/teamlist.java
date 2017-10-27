44643118b1e70d4537ff3e255be948d6ed8a7eb4
ApocalyVec <zli12@wpi.edu>
2017-10-27 17:31:47 -0400

    add method print_zli, make call in main

--- src/teamlist.java
+++ src/teamlist.java
@@ -1,10 +1,14 @@
 public class teamlist {
 
     public static void main(String[] args) {
+        this.print_zli();
         System.out.println("Jerry Brown");
         System.out.println("Michael Sidler");
-        System.out.println("Ziheng(Leo) Li");
         System.out.println("Da Xu");
         System.out.println("Ben Gillette");
+    }
+
+    public void print_zli() {
+        System.out.println("Ziheng(Leo) Li");
     }
 }
