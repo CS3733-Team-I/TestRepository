3f2a7d673116e93f4051141961e212d7c262e395
Michael <m.sidler456@gmail.com>
2017-10-27 17:31:58 -0400

    Initial commit

--- src/teamlist.java
+++ src/teamlist.java
@@ -2,9 +2,13 @@ public class teamlist {
 
     public static void main(String[] args) {
         System.out.println("Jerry Brown");
-        System.out.println("Michael Sidler");
+        printMichael();
         System.out.println("Ziheng(Leo) Li");
         System.out.println("Da Xu");
         System.out.println("Ben Gillette");
+    }
+
+    public static void printMichael() {
+        System.out.println("Michael Sidler");
     }
 }
