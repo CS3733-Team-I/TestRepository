359e74e54c6c175f8eb7131fcc4e77448abc8727
ApocalyVec <zli12@wpi.edu>
2017-10-27 17:46:49 -0400

    modify print_zli12()

--- src/teamlist.java
+++ src/teamlist.java
@@ -1,20 +1,21 @@
 public class teamlist {
 
     public static void main(String[] args) {
-        this.print_zli();
+
         System.out.println("Jerry Brown");
         System.out.println("Michael Sidler");
         System.out.println("Da Xu");
         System.out.println("Ben Gillette");
 
         jerry();
+        print_zli();
     }
 
     public static void jerry() {
         System.out.println("Jerry Branch -- Branch");
     }
 
-    public void print_zli() {
+    public static void print_zli() {
         System.out.println("Ziheng(Leo) Li");
     }
 }
