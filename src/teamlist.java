096518f302943e3f60205cbb2005354328e9e7c1
John Parrick <jfparrick@wpi.edu>
2017-10-27 17:48:40 -0400

    Merge branch 'master' of https://github.com/CS3733-Team-I/TestRepository
    # Conflicts:
    #	src/teamlist.java

--- src/teamlist.java
+++ src/teamlist.java
@@ -5,16 +5,18 @@ public class teamlist {
         System.out.println("Michael Sidler");
         System.out.println("Da Xu");
         System.out.println("Ben Gillette");
+        System.out.println("John Parrick");
         printbkg();
+        print_jp();
 
     }
     private static void printbkg(){
         System.out.println("Ben Gillette new method");
-        System.out.println("John Parrick");
+
         System.out.println("Henry Dunphy");
 
         jerry();
-        print_jp();
+
     }
 
     public static void jerry() {
